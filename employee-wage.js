17412649a981ae820aaa369b3cc8869aea5c0805
Atharva Joshi <iamatharvaj@gmail.com>
2021-05-03 11:57:45 +0530

    Calculating wage for a multiple number of days

--- employee-wage.js
+++ employee-wage.js
@@ -1,6 +1,7 @@
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
 
@@ -17,8 +18,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10)%3;
-empHrs = getWorkingHours(empCheck);
+for(let day=1; day<=NUM_OF_WORKING_DAYS; day++) {
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
 
-console.log("Employee wage for "+ empHrs +" hours is: " + empWage);
+console.log("Total employee wage for "+ empHrs +" hours is: " + empWage);
