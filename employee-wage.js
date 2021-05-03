5ff9487b119aa50e4484605861e94b417e5cd102
Atharva Joshi <iamatharvaj@gmail.com>
2021-05-03 11:57:45 +0530

    Getting employee working hours through a function

--- employee-wage.js
+++ employee-wage.js
@@ -3,19 +3,22 @@ const IS_FULL_TIME = 2;
 const WAGE_PER_HOUR = 20;
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
 
-switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;	
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return 4;
+            break;
+        case IS_FULL_TIME:
+            return 8;
+            break;	
+        default:
+            return 0;
+    }
 }
 
+let empCheck = Math.floor(Math.random()*10)%3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 
 console.log("Employee wage for "+ empHrs +" hours is: " + empWage);
