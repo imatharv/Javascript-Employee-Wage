80906fbf0f1114440a2ed104e0199dd2370ec69e
Atharva Joshi <iamatharvaj@gmail.com>
2021-05-06 11:57:45 +0530

    Calculating the employee wage w.r.t. working hours

--- employee-wage.js
+++ employee-wage.js
@@ -1,11 +1,20 @@
-console.log("The solution to employee wage problame in JavaScript");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const WAGE_PER_HOUR = 20;
 
-const IS_ABSENT = 0;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
 
-let empCheck = Math.floor(Math.random()*10)%2;
-
-if(empCheck == IS_ABSENT) {
-    console.log("Employee Absent");
-} else {
-    console.log("Employee Present");
+switch(empCheck) {
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;	
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee wage for "+ empHrs +" hours is: " + empWage);
