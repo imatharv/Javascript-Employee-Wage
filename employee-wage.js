4499f5dbfb00b7fc4c0d8b1a9b95e729b9249121
Atharva Joshi <iamatharvaj@gmail.com>
2021-06-19 00:00:30 +0530

    Storing day, hours worked and wage in a single object

--- employee-wage.js
+++ employee-wage.js
@@ -123,3 +123,24 @@ empDailyHrsMap.forEach((value, key ) => {
 console.log("Full Working Days: "+fullWorkingDays);
 console.log("Part Working Days: "+partWorkingDays);
 console.log("Non Working Days: "+nonWorkingDays);
+
+//UC 10 To store Day, Hours Worked and Wage Earned in a Single Object
+totalEmpHrs =0;
+let totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push(
+    { 
+        dayNum: totalWorkingDays,
+        dailyHours: empHrs,
+        dailyWage: calcDailyWage(empHrs),
+        toString() {
+            return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = '+this.dailyWage
+        }, 
+    }); 
+}
+console.log("UC-10 Daily Hours worked and Wage Earned: "+empDailyHrsAndWageArr);
