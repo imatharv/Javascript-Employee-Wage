362d9e795fb5052879c81b03892d395318be5fea
Atharva Joshi <iamatharvaj@gmail.com>
2021-06-19 09:00:06 +0530

    Using regex for validating the name

--- employee-payroll.js
+++ employee-payroll.js
@@ -1,6 +1,5 @@
 class EmployeePayrollData {
     id;
-    name;
     salary;
     gender;
     startDate;
@@ -14,16 +13,28 @@ class EmployeePayrollData {
     }
 
     get name() {return this._name;}
-    set name(name) {this._name = name;}
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(nameRegex.test(name))
+        this._name = name;
+        else throw 'Invalid Name!';
+    }
 
     toString() {
         return "id = "+ this.id +", name = "+this.name+", salary = "+this.salary + ", gender = "+ this.gender +", startDate = "+this.startDate;
     }
 }
-
-const employeePayrolldata = new EmployeePayrollData(1, "Mark", 30000,"M","2021-05-22");
+    
+let employeePayrolldata = new EmployeePayrollData(1, "Mark", 60000,"M","2021-05-22");
 console.log("Payroll Data: "+employeePayrolldata.toString());
-employeePayrolldata.name = "James";
-console.log("After Replacement: "+employeePayrolldata.toString());
-const newemployeePayrolldata = new EmployeePayrollData(1, "Terrisa", 60000 ,"F", "2020-05-31");
-console.log("New Payroll Data: "+newemployeePayrolldata.toString());
+
+//using try catch block to check name validation
+try{
+    employeePayrolldata.name = "james";
+    console.log(employeePayrolldata.toString())
+}catch(e){
+    console.error(e);
+}
+
+let newemployeePayrolldata = new EmployeePayrollData(1, "Terisa", 50000 ,"F", "2020-05-31");
+console.log("New Payroll Data: " + newemployeePayrolldata.toString());
